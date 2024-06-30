a841286d41c9812b678fe3f48f46111f4d0f9caf
mivan50 <s4014458@student.rmit.edu.au>
2024-06-30 16:39:54 +1000

    Added some visual enhancements as well as othe rinformation such as height and weight.

--- index.js
+++ index.js
@@ -8,20 +8,31 @@
 
 async function fetchData() {
     try {
-
         const pokemonName = document.getElementById("pokemonName").value.toLowerCase();
         const response = await fetch(`https://pokeapi.co/api/v2/pokemon/${pokemonName}`);
 
         if (!response.ok) {
-            throw new Error("Could not fetch resource");
+            throw new Error("Pokémon not found");
         }
 
         const data = await response.json();
         const pokemonSprite = data.sprites.front_default;
-        const imgElement = document.getElementById("pokemonSprite")
-
+        const pokemonWeight = data.weight / 10; // Convert weight to kg
+        const pokemonHeight = data.height / 10; // Convert height to m
+        
+        const imgElement = document.getElementById("pokemonSprite");
         imgElement.src = pokemonSprite;
-        imgElement.style.display = "block"
+        imgElement.style.display = "block";
+
+        const pokemonWeightElement = document.getElementById("pokemonWeight");
+        const pokemonHeightElement = document.getElementById("pokemonHeight");
+        
+        pokemonWeightElement.textContent = pokemonWeight;
+        pokemonHeightElement.textContent = pokemonHeight;
+
+        // Display the pokemon info container
+        const pokemonInfoElement = document.getElementById("pokemonInfo");
+        pokemonInfoElement.style.display = "block"; 
     }
     catch(error){
         console.error(error);
